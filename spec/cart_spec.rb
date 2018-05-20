e67901fd16cc17f4ce66c4ca719a680434cc3bfc
smallwordWISH <word714cs@gmail.com>
2018-05-20 23:46:18 +0800

    五本不同集的書打 20% 折扣

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -88,4 +88,36 @@ describe Cart do
       expect(@cart.calculate).to eq(1240)
     end
   end
+
+  context "第五種情境：打 20% 折扣" do
+    it "第一集買 1 本、第二集買 1 本、第三集買 1 本、第四集買 1 本、第五集買 1 本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 1, "4th": 1, "5th": 1 })
+      expect(@cart.calculate).to eq(400)
+    end
+
+    it "第一集買 1 本、第二集買 1 本、第三集買 1 本、第四集買 1 本、第五集買 2 本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 1, "4th": 1, "5th": 2 })
+      expect(@cart.calculate).to eq(500)
+    end
+
+    it "第一集買 1 本、第二集買 1 本、第三集買 1 本、第四集買 2 本、第五集買 3 本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 1, "4th": 2, "5th": 3 })
+      expect(@cart.calculate).to eq(690)
+    end
+
+    it "第一集買 1 本、第二集買 1 本、第三集買 2 本、第四集買 3 本、第五集買 4 本" do
+      @cart.add({ "1st": 1, "2nd": 1, "3rd": 2, "4th": 3, "5th": 4 })
+      expect(@cart.calculate).to eq(960)
+    end
+
+    it "第一集買 1 本、第二集買 2 本、第三集買 3 本、第四集買 4 本、第五集買 5 本" do
+      @cart.add({ "1st": 1, "2nd": 2, "3rd": 3, "4th": 4, "5th": 5 })
+      expect(@cart.calculate).to eq(1300)
+    end
+
+    it "第一集買 5 本、第二集買 3 本、第三集買 2 本、第四集買 6 本、第五集買 4 本" do
+      @cart.add({ "1st": 5, "2nd": 3, "3rd": 2, "4th": 6, "5th": 4 })
+      expect(@cart.calculate).to eq(1700)
+    end
+  end
 end
