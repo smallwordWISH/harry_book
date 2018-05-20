e391881c774045736e10bccd4e7543bf61f9329a
smallwordWISH <word714cs@gmail.com>
2018-05-20 23:31:37 +0800

    三本不同集的書打 10% 折扣

--- lib/cart.rb
+++ lib/cart.rb
@@ -10,8 +10,10 @@ class Cart
     total = 0
 
     while @ordered_items.size > 0
-      if @ordered_items.size == 2
-          total += 2 * PRICE * 0.95
+      if @ordered_items.size == 3
+        total += 3 * PRICE * 0.9
+      elsif @ordered_items.size == 2
+        total += 2 * PRICE * 0.95
       else
         total += PRICE
       end
